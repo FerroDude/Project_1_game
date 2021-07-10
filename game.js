653ce0a2a1d7c4d631e1d32b28299d70ebac4d57
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-07-10 16:17:29 +0100

    Added movement logic with gravity and jump

--- game.js
+++ game.js
@@ -2,13 +2,62 @@ class Game {
   constructor(canvas) {
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
+    this.player = new Player(this, this.canvas.width / 2, 0);
   }
   start() {
-    this.player = new Player(this, 0, this.canvas.height - );
-    this.paint();
+    //this.player = new Player(this, this.canvas.width / 2, canvas.height / 2);
+
+    this.enableControls();
+    this.loop();
+  }
+
+  enableControls() {
+    //need to add keyup and maybe change how acceleration works
+    window.addEventListener('keydown', (event) => {
+      const key = event.code;
+      switch (key) {
+        case 'ArrowRight':
+          this.player.accelerationX += 0.2;
+          break;
+        case 'ArrowLeft':
+          this.player.accelerationX -= 0.2;
+          console.log(this.player.speedX);
+          break;
+        case 'ArrowUp':
+          this.player.speedY = -20;
+          console.log(this.player.speedX);
+          break;
+      }
+    });
+    window.addEventListener('keyup', (event) => {
+      const key = event.code;
+      switch (key) {
+        case 'ArrowRight':
+        case 'ArrowLeft':
+          this.player.accelerationX = 0;
+          break;
+      }
+    });
+  }
+
+  runLogic() {
+    this.player.runLogic();
+  }
+
+  loop() {
+    window.requestAnimationFrame(() => {
+      this.runLogic();
+      this.paint();
+      this.loop();
+    });
+  }
+
+  clearScreen() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
   paint() {
+    this.clearScreen();
     this.player.paint();
   }
 }
--- player.js
+++ player.js
@@ -3,12 +3,53 @@ class Player {
     this.game = game;
     this.x = x;
     this.y = y;
+    this.speedY = 0;
+    this.speedX = 0;
+    this.maxSpeed = 3;
+    this.accelerationX = 0;
+    this.accelerationY = 0;
     this.width = 30;
     this.height = 50;
   }
+
+  runLogic() {
+    const friction = 0.1;
+    const GRAVITY = 0.3;
+
+    this.speedX += this.accelerationX;
+
+    if (this.speedX > 0) {
+      this.speedX -= friction;
+    } else if (this.speedX < 0) {
+      this.speedX += friction;
+    }
+
+    if (this.speedX > this.maxSpeed) {
+      this.speedX = this.maxSpeed;
+    } else if (this.speedX < -this.maxSpeed) {
+      this.speedX = -this.maxSpeed;
+    }
+    this.speedY += GRAVITY;
+
+    this.x += this.speedX;
+    this.y += this.speedY * GRAVITY;
+
+    //this.speedY += this.accelerationY; there is no up or down movement, except jump
+
+    /*     if (this.speedX > 0) {
+      this.speedX -= friction;
+    } else if (this.speedX < 0) {
+      this.speedX += friction;
+    }
+    this.x += this.speedX;
+   */
+  }
+
   paint() {
     const context = this.game.context;
+    context.save();
     context.fillStyle = 'grey';
     context.fillRect(this.x, this.y, this.width, this.height);
+    context.restore();
   }
 }
